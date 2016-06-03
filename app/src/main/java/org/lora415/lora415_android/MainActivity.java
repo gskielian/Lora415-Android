7eb9631c35ac9896257ed16234a02e537909dd15
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-06-02 23:07:51 -0700

    add real time listener and updater

--- app/src/main/java/org/lora415/lora415_android/MainActivity.java
+++ app/src/main/java/org/lora415/lora415_android/MainActivity.java
@@ -5,11 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    private String TAG = "MainActivity";
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +34,51 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tv = (TextView) findViewById(R.id.current_message);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Greg");
+        myRef.setValue("Hello, World!");
+
+
+
+        begin_live_data_link();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+
+                // Write a message to the database
+                myRef = database.getReference("Greg");
+                myRef.setValue("Hello, World!");
+
+                Snackbar.make(view, "Message sent to Firebase", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void begin_live_data_link() {
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                tv.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 
     @Override
